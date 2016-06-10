b836e50438e5f28c818b33a4c9c29205523f3d4b
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-06-10 12:13:58 +0300

    Suppression Demo.

--- src/main/java/exceptions/demo/DBConnection.java
+++ src/main/java/exceptions/demo/DBConnection.java
@@ -4,6 +4,10 @@ import java.sql.SQLException;
 
 public class DBConnection implements AutoCloseable {
     public void close() throws SQLException {
+        throw new RuntimeException("qqq");
+    }
 
+    public void getSmth() throws SQLException {
+        throw new SQLException();
     }
 }
--- src/main/java/exceptions/demo/ExceptionsDemo.java
+++ src/main/java/exceptions/demo/ExceptionsDemo.java
@@ -9,34 +9,35 @@ import java.sql.SQLException;
 public class ExceptionsDemo {
     public static void main(String[] args) throws EmployeeNotFoundException {
         EmployeeDAO employeeDAO = new EmployeeDAO();
-        String name = employeeDAO.getEmployeeNameById(3);
+        try {
+            String name = employeeDAO.getEmployeeNameById(3);
+        } catch (RuntimeException e) {
+            System.out.println(e.getSuppressed());
+        }
         System.out.println("done");
     }
 }
 
 class EmployeeDAO {
     public String getEmployeeNameById(int id) throws EmployeeNotFoundException {
-
-        try (DBConnection dbConnection = getConnection();
-             InputStream is = new FileInputStream("")
-        ) {
-            //.....
-        } catch (SQLException | IOException e) {
+        try (DBConnection dbConnection = getConnection()) {
+            dbConnection.getSmth();
+            //business - logic
+        } catch (SQLException e) {
             //0. Logging
             //1. Return default
             //2. Retrying
             //3. Fail-over
             throw new EmployeeNotFoundException("Can't ", e);
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
-        System.out.println("done...");
+        //System.out.println("done...");
         return "";
     }
 
     //@Retryable(timeout = 10_000, count = 10)
     private DBConnection getConnection() throws SQLException {
-        throw new SQLException("RRRRR");
+        //throw new SQLException("RRRRR");
+        return new DBConnection();
     }
 }
